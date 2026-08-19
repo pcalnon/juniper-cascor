36ca95c1c1b05f6d032a419de3125c7de1cbca5d
Paul Calnon <paul.calnon@gmail.com>
2026-08-19 04:47:35 -0500

    fix(rename_snapshots): prepend file prefix to renamed snapshot files
    Updates the snapshot renaming script to include a file prefix before the UUID in the renamed files. This change enhances the clarity of the file naming convention, making it easier to identify the source of the snapshots. Additionally, minor adjustments to comments were made for improved readability.

--- util/rename_snapshots.bash
+++ util/rename_snapshots.bash
@@ -26,26 +26,27 @@ DEST_DIR="src/cascor_snapshots"
 
 ########################################################################################################################################################################################################################
 # Define Operational Constants
-APPL_DIR="${ROOT_DIR}/${LANG_DIR}/${PROJ_DIR}/${REPO_DIR}"  #; echo "APPL_DIR: ${APPL_DIR}"
-ORIGIN_DIR="${APPL_DIR}/${SRCE_DIR}"                        #; echo "ORIGIN_DIR: ${ORIGIN_DIR}"
-TARGET_DIR="${APPL_DIR}/${DEST_DIR}"                        #; echo "TARGET_DIR: ${TARGET_DIR}"
-FILE_STUB="snapshot_2026"                                   #; echo "FILE_STUB: ${FILE_STUB}"
-FILE_EXT=".h5"                                              #; echo "FILE_EXT: ${FILE_EXT}"
+APPL_DIR="${ROOT_DIR}/${LANG_DIR}/${PROJ_DIR}/${REPO_DIR}"  # ; echo "APPL_DIR: ${APPL_DIR}"
+ORIGIN_DIR="${APPL_DIR}/${SRCE_DIR}"                        # ; echo "ORIGIN_DIR: ${ORIGIN_DIR}"
+TARGET_DIR="${APPL_DIR}/${DEST_DIR}"                        # ; echo "TARGET_DIR: ${TARGET_DIR}"
+FILE_PREF="cascor"                                          # ; echo "FILE_PREF: ${FILE_PREF}"
+FILE_STUB="snapshot_2026"                                   # ; echo "FILE_STUB: ${FILE_STUB}"
+FILE_EXT=".h5"                                              # ; echo "FILE_EXT: ${FILE_EXT}"
 
 
 ########################################################################################################################################################################################################################
 # Do the things
 cd "${ORIGIN_DIR}" || exit
 
-for i in "${ORIGIN_DIR}/${FILE_STUB}"*; do   # ; echo "i: ${i}"
-    [[ -f "${i}" ]] || continue              # Verify file exists
-    UUID="$(uuidgen)"                        # ; echo "uuid: ${UUID}"
-    m="$(basename "${i}")"                   # ; echo "m: ${m}"
-    j="$(echo "${m}" | tr -d "Z")"           # ; echo "j: ${j}"
-    k="$(echo "${j}" | tr "T" "_")"          # ; echo "k: ${k}"
-    l="$(basename -s "${FILE_EXT}" "${k}")"  # ; echo "l: ${l}"
-    n="${l}_${UUID}${FILE_EXT}"              # ; echo "n: ${n}"
-    p="${TARGET_DIR}/${n}"                   # ; echo "p: ${p}"
+for i in "${ORIGIN_DIR}/${FILE_STUB}"*; do    # ; echo "i: ${i}"
+    [[ -f "${i}" ]] || continue               # Verify file exists
+    UUID="$(uuidgen)"                         # ; echo "uuid: ${UUID}"
+    m="$(basename "${i}")"                    # ; echo "m: ${m}"
+    j="$(echo "${m}" | tr -d "Z")"            # ; echo "j: ${j}"
+    k="$(echo "${j}" | tr "T" "_")"           # ; echo "k: ${k}"
+    l="$(basename -s "${FILE_EXT}" "${k}")"   # ; echo "l: ${l}"
+    n="${FILE_PREF}_${l}_${UUID}${FILE_EXT}"  # ; echo "n: ${n}"
+    p="${TARGET_DIR}/${n}"                    # ; echo "p: ${p}"
     echo -ne "Moving File: ${m}\n"
     echo -ne "Pre-Move check: \"$(ls "${p}" 2>/dev/null)\"\n"
     echo -ne "mv ${i} ${p}\n"
